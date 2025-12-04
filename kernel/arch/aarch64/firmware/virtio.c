fd9ee7d96c197f80f906f00bc6a2815b7bb238dd
Valerio <83879763+valerioedu@users.noreply.github.com>
2025-12-04 18:00:44 +0100

    Update virtio.c

--- kernel/arch/aarch64/firmware/virtio.c
+++ kernel/arch/aarch64/firmware/virtio.c
@@ -11,21 +11,6 @@
 
 static u64 virtio_blk_base = 0;
 
-static volatile virtq_desc* desc;
-static volatile virtq_avail* avail;
-static volatile virtq_used* used;
-
-static spinlock_t virtio_lock = 0;
-
-// Simple descriptor allocator
-static u16 free_desc_stack[QUEUE_SIZE];
-static int free_desc_top = 0;
-
-typedef struct {
-    virtio_blk_req req;     
-    volatile u8 status;     
-} req_slot_t;
-
 void virtio_init() {
     kprintf("[ [CVirtIO [W] Scanning...\n");
 
@@ -42,27 +27,4 @@ void virtio_init() {
     if (!virtio_blk_base) return;
 
     u64 base = virtio_blk_base;
-    mmio_write32(base + VIRTIO_MMIO_STATUS, 0); // Reset
-    mmio_write32(base + VIRTIO_MMIO_STATUS, VIRTIO_STATUS_ACK | VIRTIO_STATUS_DRV);
-    mmio_write32(base + VIRTIO_MMIO_GUEST_PAGE_SIZE, 4096);
-
-    // Queue Setup
-    mmio_write32(base + VIRTIO_MMIO_QUEUE_SEL, 0);
-    if (mmio_read32(base + VIRTIO_MMIO_QUEUE_NUM_MAX) == 0) return;
-    mmio_write32(base + VIRTIO_MMIO_QUEUE_NUM, QUEUE_SIZE);
-
-    u64 page = pmm_alloc_frame();
-    memset((void*)page, 0, 4096);
-
-    desc  = (virtq_desc*) page;
-    avail = (virtq_avail*) (page + QUEUE_SIZE * 16);
-    used  = (virtq_used*) (page + 4096 - sizeof(virtq_used));
-
-    mmio_write32(base + VIRTIO_MMIO_QUEUE_PFN, page >> 12);
-
-    for (int i = 0; i < QUEUE_SIZE; i++) free_desc_stack[i] = i;
-    free_desc_top = QUEUE_SIZE;
-
-    mmio_write32(base + VIRTIO_MMIO_STATUS, VIRTIO_STATUS_ACK | VIRTIO_STATUS_DRV | VIRTIO_STATUS_FEAT_OK | VIRTIO_STATUS_DRV_OK);
-    kprintf("[ [CVirtIO [W] Initialized (Polling Mode).\n");
 }
