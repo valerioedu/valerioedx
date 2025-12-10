b32131db7c9b46779b6f61f6ab63cbad3106123e
Valerio <83879763+valerioedu@users.noreply.github.com>
2025-12-10 02:40:45 +0100

    Comment on target inode ops

--- kernel/lib/vfs.h
+++ kernel/lib/vfs.h
@@ -19,6 +19,47 @@ typedef struct {
     struct vfs_node* (*finddir)(struct vfs_node* file, const char *name);
 } inode_ops;
 
+/*
+Linux operations, at the end inode_ops should look something like this
+
+struct inode_operations {
+    struct file_operations * default_file_ops;
+    int (*create) (struct inode *,const char *,int,int,struct inode **);
+    int (*lookup) (struct inode *,const char *,int,struct inode **);
+    int (*link) (struct inode *,struct inode *,const char *,int);
+    int (*unlink) (struct inode *,const char *,int);
+    int (*symlink) (struct inode *,const char *,int,const char *);
+    int (*mkdir) (struct inode *,const char *,int,int);
+    int (*rmdir) (struct inode *,const char *,int);
+    int (*mknod) (struct inode *,const char *,int,int,int);
+    int (*rename) (struct inode *,const char *,int, struct inode *,
+               const char *,int, int);
+    int (*readlink) (struct inode *,char *,int);
+    int (*follow_link) (struct inode *,struct inode *,int,int,struct inode **);
+    int (*readpage) (struct inode *, struct page *);
+    int (*writepage) (struct inode *, struct page *);
+    int (*bmap) (struct inode *,int);
+    void (*truncate) (struct inode *);
+    int (*permission) (struct inode *, int);
+    int (*smap) (struct inode *,int);
+};
+
+struct file_operations {
+    int (*lseek) (struct inode *, struct file *, off_t, int);
+    int (*read) (struct inode *, struct file *, char *, int);
+    int (*write) (struct inode *, struct file *, const char *, int);
+    int (*readdir) (struct inode *, struct file *, void *, filldir_t);
+    int (*select) (struct inode *, struct file *, int, select_table *);
+    int (*ioctl) (struct inode *, struct file *, unsigned int, unsigned long);
+    int (*mmap) (struct inode *, struct file *, struct vm_area_struct *);
+    int (*open) (struct inode *, struct file *);
+    void (*release) (struct inode *, struct file *);
+    int (*fsync) (struct inode *, struct file *);
+    int (*fasync) (struct inode *, struct file *, int);
+    int (*check_media_change) (kdev_t dev);
+    int (*revalidate) (kdev_t dev);
+};*/
+
 typedef struct vfs_node {
     char name[32];
     u32 flags;
