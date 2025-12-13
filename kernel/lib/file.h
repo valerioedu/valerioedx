3b451ad02bded19db4f92d4c9c6d48d9aba217ef
Valerio <83879763+valerioedu@users.noreply.github.com>
2025-12-13 17:06:11 +0100

    add file.c functions to header

--- kernel/lib/file.h
+++ kernel/lib/file.h
@@ -10,4 +10,10 @@ typedef struct file {
     int ref_count;
 } file_t;
 
+int fd_alloc();
+void fd_free(int fd);
+file_t* file_new(inode_t* inode, u32 flags);
+void file_close(file_t* file);
+file_t* fd_get(int fd);
+
 #endif
