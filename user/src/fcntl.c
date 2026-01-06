70d52d6ee0b78cfb4ed4a552022f970d7af62c76
Valerio <83879763+valerioedu@users.noreply.github.com>
2026-01-06 20:09:17 +0100

    open syscall

---
+++ user/src/fcntl.c
@@ -0,0 +1,10 @@
+#include <fcntl.h>
+
+int open(const char *path, int oflag) {
+    register const char *x0 asm("x0") = path;
+    register int x1 asm("x1") = oflag;
+    register unsigned short x8 asm("x8") = 5;
+    asm volatile("svc #0"
+        : "+r"(x0) : "r"(x1), "r"(x8) : "memory");
+    return (int)x0;
+}
