6bb897b2e8eb7978626c76cef2fdcaee40573643
Valerio <83879763+valerioedu@users.noreply.github.com>
2026-01-12 02:13:10 +0100

    Posix standard dirent struct

--- user/lib/dirent.h
+++ user/lib/dirent.h
@@ -4,13 +4,13 @@
 #include <sys/types.h>
 #include <stdint.h>
 
-typedef struct {
+struct dirent {
     uint64_t d_ino;
     uint64_t d_off;
     uint16_t d_reclen;
     uint8_t d_type;
     char d_name[256];
-} dirent_t;
+};
 
 ssize_t getdirentries(int fildes, char buf[], size_t nbytes, int64_t *basep);
 
