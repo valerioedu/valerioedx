78d011e54777af2743c8c6bc89b2ff7fbd13c271
Valerio <83879763+valerioedu@users.noreply.github.com>
2026-01-18 01:04:47 +0100

    Add stdbool.h and update stddef.h and sys/wait.h
    Introduced a new stdbool.h header for boolean type definitions. Added the offsetof macro to stddef.h. Defined WNOHANG, WUNTRACED, and WCONTINUED macros in sys/wait.h to support wait options.

--- user/lib/stddef.h
+++ user/lib/stddef.h
@@ -2,6 +2,7 @@
 #define STDDEF_H
 
 #define NULL ((void*)0)
+#define offsetof(type, member) ((size_t) &((type *)0)->member)
 
 typedef unsigned long size_t;
 typedef long ptrdiff_t;
--- user/lib/sys/wait.h
+++ user/lib/sys/wait.h
@@ -3,6 +3,10 @@
 
 #include <sys/types.h>
 
+#define WNOHANG    0x00000001
+#define WUNTRACED  0x00000002
+#define WCONTINUED 0x00000008
+
 pid_t wait(int *stat_loc);
 pid_t waitpid(pid_t pid, int *stat_loc, int options);
 
